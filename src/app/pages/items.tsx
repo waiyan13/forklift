be0ce608e48919b496b6ccf768867c5c90281c8a
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-05 12:47:45 +0700

    feat[Item]: Add error boundary and suspense for data fetching

--- src/app/pages/items.tsx
+++ src/app/pages/items.tsx
@@ -1,5 +1,39 @@
-function ItemsPage() {
-  return <div>Items</div>;
+import { Suspense } from "react";
+
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { columns } from "@/features/items/components/columns";
+import ItemHeader from "@/features/items/components/header";
+import ItemTable from "@/features/items/components/table";
+import { DataTableError } from "@/components/data-table-error";
+import { DataTableSkeleton } from "@/components/data-table-skeleton";
+
+function ItemPage() {
+  return (
+    <div className="flex w-full flex-col space-y-4">
+      <ItemHeader />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            // oxlint-disable-next-line  eslint-plugin-react-perf/jsx-no-new-function-as-prop
+            fallbackRender={() => (
+              // oxlint-disable-next-line  eslint-plugin-react-perf/jsx-no-new-array-as-prop
+              <DataTableError columns={columns} data={[]} />
+            )}
+            onReset={reset}
+          >
+            <Suspense
+              // oxlint-disable-next-line eslint-plugin-react-perf/jsx-no-new-array-as-prop, eslint-plugin-react-perf/jsx-no-jsx-as-prop
+              fallback={<DataTableSkeleton columns={columns} data={[]} />}
+            >
+              <ItemTable />
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </div>
+  );
 }
 
-export default ItemsPage;
+export default ItemPage;
