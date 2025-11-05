1f5e776db52b6270cddf03a35f1a1a7bc328f990
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2025-11-05 11:27:07 +0700

    feat[Item]: Add form to add new item

---
+++ src/features/items/components/new-item-form.tsx
@@ -0,0 +1,59 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { useNewItemForm } from "@/features/items/hooks/useNewItemForm";
+
+const sleep = () => new Promise((resolve) => setTimeout(() => resolve, 1000));
+
+function NewItemForm() {
+  const form = useNewItemForm();
+  const { control, handleSubmit } = form;
+
+  const onNewItemSubmit = async () => {
+    await sleep();
+  };
+
+  return (
+    <Form {...form}>
+      <form id="new-item-form" onSubmit={handleSubmit(onNewItemSubmit)}>
+        <div className="flex flex-col space-y-4 px-4">
+          <FormField
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={control}
+            name="unit"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Unit</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
+}
+
+export default NewItemForm;
